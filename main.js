2ee191c28e3a478bc1c6524ad296c1aa33e74de2
dimitroffangel <angeldimitrov05@gmail.com>
2018-08-07 16:04:37 +0300

    testing

---
+++ main.js
@@ -0,0 +1,42 @@
+console.log('yeeee');
+var colorButton = document.getElementById('colorButton');
+var canvas = document.getElementById('myCanvas');
+var context = canvas.getContext('2d');
+
+var mouseState = {
+    x:0,
+    y:0,
+    isPressed:false
+};
+var radius = 5;
+
+function drawDot(){
+    context.fillStyle = colorButton.value;
+    context.beginPath();
+    context.arc(mouseState.x, mouseState.y, radius, 0, 360);
+    context.closePath();
+    context.fill();
+
+    console.log(mouseState.x + " ; " + mouseState.y);
+}
+
+function main(){
+    if(mouseState.isPressed)
+        drawDot();
+}
+
+document.onmousemove = function(event){
+    mouseState.x = event.pageX;
+    mouseState.y = event.pageY;
+
+    if(mouseState.isPressed)
+        drawDot();
+}
+
+document.onmousedown = function(){ 
+    mouseState.isPressed = true;
+}
+
+document.onmouseup = function(){
+    mouseState.isPressed = false;
+}
