9ec16ffb02bca4e22b34331ad178dd19952c9c6e
dimitroffangel <angeldimitrov05@gmail.com>
2018-09-01 14:31:33 +0300

    register is now working with pug

--- Back-End_Side/server.js
+++ Back-End_Side/server.js
@@ -1,22 +1,64 @@
 const express = require('express');    
 const fs = require('fs');
 const bodyParser = require('body-parser');
-
+const ensureAuthentication = require('connect-ensure-login');
 
 var passport = require('passport');
 var Strategy= require('passport-local').Strategy;
   
 var cookieParser = require('cookie-parser');
 var session = require("express-session");
-  
+
+
 var frontEndFolder = '../Front-End_Side';
 var currentPageName = frontEndFolder + '/Pages/drawingGround.html';
 
 const items = [
-    { id: 1, username: 'nikola', password: "1", displayName:'DiffKolyo', loggedIn:false, emails: [ { value: 'diffNik@example.com' } ]},    
-  { id: 2, username: 'jack', password: 'secret', displayName: 'Jack', loggedIn:false, emails: [ { value: 'jack@example.com' } ] }
-, { id: 3, username: 'jill', password: 'birthday', displayName: 'Jill', loggedIn:false, emails: [ { value: 'jill@example.com' } ] }
+    { id: 1, username: 'nikola', password: "1", displayName:'DiffKolyo', loggedIn:false, emails: [ 'diffNik@example.com' ]},    
+  { id: 2, username: 'jack', password: 'secret', displayName: 'Jack', loggedIn:false, emails: [ 'jack@example.com' ] }
+, { id: 3, username: 'jill', password: 'birthday', displayName: 'Jill', loggedIn:false, emails: [ 'jill@example.com' ] }
 ];
+
+function openFile(response, fileDirectory, contentType){
+    fs.readFile(fileDirectory, (err, data) => {
+        if(err)
+            throw err;
+        
+        response.writeHeader(200, {'Content-Type': contentType})
+        response.write(data);
+        response.end();
+    });
+}
+
+function findUsername(username){
+    for(var i = 0, length = items.length; i < length; i++){
+        if(username == items[i].username)
+            return true;
+    }
+
+    return false;
+}
+
+function findEmail(email){
+    var hasMatch;
+    items.forEach((item) => {
+        console.log(item.emails );
+        item.emails.forEach((mail) =>{
+            if(mail === email){
+                hasMatch = true;
+                return;
+            }
+
+            console.log(mail);
+        });
+
+        console.log(hasMatch + ' trying to find a result from the mail match');
+        if(hasMatch)
+            return;
+    });
+
+    return hasMatch;
+}
 
 function findById(id, cb){
     process.nextTick(() =>{
@@ -42,16 +84,19 @@ function findByUsername(username, cb){
 }
 
 function main(){
+
     passport.use(new Strategy(
         (username, password, cb) =>{
           console.log('strategizing');
           findByUsername(username, function(err, user) {
             if (err) { return cb(err); }
             if (!user) { return cb(null, false); }
-            if (user.password != password) { return cb(null, false); }
+            if (user.password != password) { return cb(null, false, {message:'invalid data'}); }
             return cb(null, user);
           });
     }));
+
+    //passport.authenticate('local', {failureFlash: 'Invalid user data'});
 
     passport.serializeUser((user, cb) =>{
         console.log('serializing.......');
@@ -69,56 +114,81 @@ function main(){
       
     const app = express();
     
-    app.use(express.static('../Front-End_Side'));
+    app.set('views', '../Front-End_Side/views');
+    app.set('view engine', 'pug');
+
+    //app.use(express.static('../Front-End_Side'));
     app.use(require('morgan')('combined'));
     app.use(cookieParser());
+    app.use(bodyParser.json());
     app.use(bodyParser.urlencoded({ extended: true }));
     app.use(session({ secret: 'keyboard cat', resave: false, saveUninitialized: false }));
 
     app.use(passport.initialize());
     app.use(passport.session());
     
-    app.get('/', (request, response) =>{
-        // var q = request.query.q,
-        //     page = parseInt(request.query.page, 10) || 1,
-        //     size = parseInt(request.query.size, 10) || 10;
-        // var result = items;
+    app.get('/', (request, response) => {
+            var q = request.query.q,
+            page = parseInt(request.query.page, 10) || 1,
+            size = parseInt(request.query.size, 10) || 10;
+        var result = items;
 
-        // if(q){
-        //     q = q.toLowerCase();
+        if(q){
+            q = q.toLowerCase();
 
-        //     result = result.filter((item) => item.name.toLowerCase().includes(q));
-        // }
-        // result = result.slice((page-1)*size, page*size);
+            result = result.filter((item) => item.name.toLowerCase().includes(q));
+        }
+        result = result.slice((page-1)*size, page*size);
 
-        // response.send(result);
-
-        fs.readFile(currentPageName, (error, html) => {
-            if(error)
-                throw error;
-            response.writeHeader(200, {'Content-Type': 'text/html'});
-            response.write(html);
-            response.end();
-        });
+        response.send(result);
     });
 
+    app.get('/drawingStudio', ensureAuthentication.ensureLoggedIn(),
+            (request, response) => {
+                openFile(response, currentPageName, 'text/html');
+    });
+
+    app.get('/userProfile', ensureAuthentication.ensureLoggedIn(),
+            (request, response) => {
+                response.send('Dis be your profile');
+    });
 
     app.get('/register', (request, response) => {
-        fs.readFile(frontEndFolder + '/Pages/LoginPage.html', (err, html) =>{
-            if(err)
-                throw err;
-        
-            response.writeHeader(200, {'Content-Type': 'text/html'});
-            response.write(html);
-            response.end();
-        });
+        response.render('register');
+        //openFile(response, frontEndFolder + '/Pages/registerPage.html', 'text\html');
+    });
+
+    app.post('/register', (request, response) => {
+        console.log(request.body);
+
+        var userData = request.body;
+
+        console.log(items);
+
+        if(findUsername(userData.username)){
+            response.render('register', {failedUsernameMessage:'username already taken'});
+            return;
+        }
+
+        if(findEmail(userData.email)){
+            response.render('register', {failedEmailMessage: 'e-mail already taken'});
+            return;            
+        }
+
+        console.log('coming after something');
+        items.push({id:items.length+1, username:userData.username, 
+                    password:userData.password, emails: [userData.email]})
+        response.redirect('login');
+    });
+
+    app.get('/login', (request, response) => {
+        openFile(response, frontEndFolder + '/Pages/LoginPage.html', 'text/html');
     });
     
-    app.post('/register', passport.authenticate('local', { failureRedirect: '/register' }),
-                (req, res) => {
-                console.log('redirecting.......');
-                res.redirect('/');
-    });
+    app.post('/login', passport.authenticate('local', { failureRedirect: '/login',
+                                                        successReturnToOrRedirect: '/drawingStudio',
+                                                        failureFlash: false,
+                                                        failureMessage: 'failed'}));
 
     app.get('/api/items', (request, response) => {
         response.send(items);
